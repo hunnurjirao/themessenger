c527abdb3af6d664a717a9f95579974b4a4cb8cb
K HUNNURJI RAO <hunnurjirao2000@gmail.com>
2021-03-03 16:45:39 +0530

    The Messenger website hosting

--- src/app.js
+++ src/app.js
@@ -13,15 +13,20 @@ const auth = require('./middleware/auth');
 
 const port = process.env.PORT || 4000;
 
+const INDEX = "../templates/views/index.html"
+
+const server = app.use((req, res) => res.sendFile(__dirname))
+    .listen(port, () => console.log(`Listening on ${port}`));
+
 
 require("./db/conn");
 const Register = require("./models/registers");
 
 const { json } = require("express");
 
-const server = app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
-})
+// const server = app.listen(port, () => {
+//     console.log(`Listening to port ${port}`);
+// })
 
 const template_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
